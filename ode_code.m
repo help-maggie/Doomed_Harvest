4bc87ba5ed68db41f0b22c1bf3cc4850e985d630
Maggie Sullens <70082590+help-maggie@users.noreply.github.com>
2023-07-18 16:32:37 -0400

    Added code files

---
+++ ode_code.m
@@ -0,0 +1,140 @@
+clear
+
+%%%%%% Inital population numbers %%%%%%
+x_0=100000;
+N_0=500;
+S_0=0;
+R_0=0;
+
+
+initial_vals=[x_0 N_0 S_0 R_0];
+
+
+%% 
+%%%%%%%%%%%% Solving ODE %%%%%%%%%%%%
+% options = odeset('RelTol',1e-8,'AbsTol',1e-10, InitialStep=1);
+[t,y] = ode45(@ode_system2_2,[0 1000],initial_vals);
+
+%%
+%%%%%%%%%%% Analysis %%%%%%%%%%%%
+r_1=.05; %make sure matches in ode_system2_2
+r_2=.05; %make sure matches in ode_system2_2
+r_3=.05; %make sure matches in ode_system2_2
+
+
+K=150000; %make sure matches in ode_system2_2
+
+
+inv_stress_people_dec=zeros;
+for i = 1:length(t)-1
+inv_stress_people_dec(i+1)=1/((r_2*(K-y(i+1,1))/K) + r_3*y(i+1,1)/K + psiP_R(y(i+1,4)) + gammaP_N(y(i+1,2)));
+end
+
+%% K(t) case%%
+% K_0=150000; %make sure matches in ode_system2_2
+% b=0.007;
+% 
+% inv_stress_people_dec=zeros;
+% for i = 1:length(t)-1
+% inv_stress_people_dec(i+1)=1/((r_2*((K_0*(1-b)^t(i+1))-y(i+1,1))/(K_0*(1-b)^t(i+1))) + r_3*y(i+1,1)/(K_0*(1-b)^t(i+1)) + psiP_R(y(i+1,4)) + gammaP_N(y(i+1,2)));
+% end 
+
+%%
+
+initial_vals_2=[x_0 N_0 0 S_0 R_0];
+[t_2,y_2] = ode45(@Analysis_System,[0 1000],initial_vals_2);
+
+people_never_stressed=zeros;
+for i = 1:length(t_2)-1
+people_never_stressed(i+1)=(deltaP_S(y_2(i+1,4)))*y_2(i+1,2)*(t_2(i+1)-t_2(i));
+end
+
+avg_time_stressed=mean(inv_stress_people_dec)
+
+total_people_never_stressed=sum(people_never_stressed)
+
+total_people_that_were_stressed=N_0-total_people_never_stressed
+ 
+max_stressed=max(findpeaks(y(:,3)))
+
+
+
+tspan=zeros;
+tspan_2=zeros;
+for i = 1:length(t)
+    if y(i,3)>1
+        tspan(i)= t(i);
+    end
+end
+
+time_of_stress=tspan(end)-tspan(1)
+
+%%
+
+% Plot solution
+
+figure("Name","Human Population")
+hold on
+plot(t,y(:,2),'color', 'b')
+plot(t,y(:,3),'color', 'r')
+plot(t,y(:,4),'color', '#EDB120')
+xlim([0 200])
+xlabel('t')
+ylabel('number of people')
+legend('Non-stressed', 'Stressed', 'Recovered')
+
+subplot(1,2,1)
+title("Fish Population")
+hold on
+plot(t,y(:,1))
+xlim([0 200])
+xlabel('t')
+ylabel('number of fish')
+
+subplot(1,2,2)
+title("Human Population")
+hold on
+plot(t_2,y_2(:,2),'color', 'b')
+plot(t_2,y_2(:,3),'color', 'm')
+plot(t_2,y_2(:,4),'color', 'r')
+plot(t_2,y_2(:,5),'color', '#EDB120')
+xlim([0 200])
+xlabel('t')
+ylabel('number of people')
+legend('Non-stressed Never Stressed','Non-stressed but Stressed Before', 'Stressed', 'Recovered')
+
+
+
+%%
+alpha_1=4;%make sure matches in ode_system2_2
+alpha_2=4;%make sure matches in ode_system2_2
+alpha_3=4;%make sure matches in ode_system2_2
+alpha_4=4;%make sure matches in ode_system2_2
+
+phi_1=1;%make sure matches in ode_system2_2
+phi_2=1;%make sure matches in ode_system2_2
+phi_3=1;%make sure matches in ode_system2_2
+phi_4=1;%make sure matches in ode_system2_2
+
+c= .01;
+    function out = betaP_S(S)
+        z = 1:floor(S);
+        out =sum((1./(1+exp(-(z-4)/1))).*poisspdf(z,.01*S));
+    end
+
+    function out = gammaP_N(N)
+        z = 1:floor(N);
+        out = sum((1./(1+exp(-(z-4)/1))).*poisspdf(z,.01*N));
+    end
+
+    function out = psiP_R(R)
+        z = 1:floor(R);
+        out = sum((1./(1+exp(-(z-4)/1))).*poisspdf(z,.01*R));
+     end
+
+   function out = deltaP_S(S)
+        z = 1:floor(S);
+        out = sum((1./(1+exp(-(z-4)/1))).*poisspdf(z,.01*S));
+   end
+
+
---
+++ ode_system2_2.m
@@ -0,0 +1,106 @@
+function [dydt]= ode_system2_2(t,y)
+dydt=zeros(4,1);
+x_0=y(1);
+%%
+%%%%%% Fishing Effort Options %%%%%%
+
+%%%% (1) manpower (# fishing) %%%%
+  % E= @(N,S) N+S;
+ 
+%%%% (2) increasing desperation %%%%
+ rho=3;
+ lambda=.07;
+ mu=x_0/2; % shifts curve to center around half of the intial fish population 
+ E= @(N,S,x) N+rho*(S)/(1+exp(lambda*x-mu));
+ 
+ 
+ 
+%%
+%%%%%% catchability %%%%%%
+q=0.001;
+
+
+%%
+%%%%%% Fish Population ODE Options %%%%%%
+
+% K=150000;
+% r=-.01;
+% dxdt=@(x,E) r*x*(1-(x/K))-q*E*x; %density dependent decrease where the smaller the population is, the faster the decrease
+
+r=.3;
+b=0.007; %rate of decrease
+K_0=150000;
+K=K_0*(1-b)^t;
+dxdt=@(x,E) r*x*(1-(x/K))-q*E*x; %decreasing carrying cpacity
+
+% r=.3;
+% m=.35; %proportion of death due to reason such as disease
+% K=150000;
+% dxdt=@(x,E) r*x*(1-(x/K))-m*x-q*E*x; %density dependent removal such as disease
+
+
+
+
+%% 
+%%%%%%%%%%%% expected values of switching state based on number of contacts  %%%%%%%%%%%%
+c=.01;
+
+alpha_1=4;
+alpha_2=4;
+alpha_3=4;
+alpha_4=4;
+
+phi_1=1;
+phi_2=1;
+phi_3=1;
+phi_4=1;
+    function out = gammaP_N(N)
+        z = 1:floor(N);
+        out = sum((1./(1+exp(-(z-alpha_1)/phi_1))).*poisspdf(z,c*N));
+    end
+
+    function out = betaP_S(S)
+        z = 1:floor(S);
+        out =sum((1./(1+exp(-(z-alpha_2)/phi_2))).*poisspdf(z,c*S));
+    end
+
+    function out = psiP_R(R)
+        z = 1:floor(R);
+        out = sum((1./(1+exp(-(z-alpha_3)/phi_3))).*poisspdf(z,c*R));
+    end
+
+    function out = deltaP_S(S)
+        z = 1:floor(S);
+        out = sum((1./(1+exp(-(z-alpha_4)/phi_4))).*poisspdf(z,c*S));
+    end
+
+ 
+% syms z;
+% gammaP_N = @(N) symsum(1/(1+exp(-(z-alpha_1)/phi_1))*poisspdf(z,c*N),z,1,floor(N));
+% betaP_S = @(S) sysmsum((1/(1+exp(-(z-alpha_2)/phi_2)))*poisspdf(z,c*S),z,1,floor(S));
+% psiP_R = @(R) sysmsum((1/(1+exp(-(z-alpha_3)/phi_3)))*poisspdf(z,c*R),z,1,floor(R));
+% deltaP_S = @(S) sysmsum((1/(1+exp(-(z-alpha_4)/phi_4)))*poisspdf(z,c*S),z,1,floor(S));
+
+%%
+%%%%%%%%%%%%% R_0 %%%%%%%%%%%%
+
+
+%% 
+%%%%%%%%%%%% ODE System %%%%%%%%%%%%
+% y(1)=x; %(fish pop)
+% y(2)=N; %(non-stressed pop)
+% y(3)=S; %(stressed pop),
+% y(4)=R; %(recovered pop)
+
+
+r_1=.05;
+r_2=.05;
+r_3=.05;
+
+
+dydt(1)=dxdt(y(1),E(y(2),y(3),y(1))); %fish population ode
+dydt(2)=((r_3*y(1)/K)+ gammaP_N(y(2)))*y(3)-((r_1*(K-y(1))/K)+betaP_S(y(3)) + deltaP_S(y(3)))*y(2); %non-stressed population ode
+dydt(3)=((r_1*(K-y(1))/K)+betaP_S(y(3)))*y(2) - ((r_2*(K-y(1))/K) + r_3*y(1)/K + psiP_R(y(4)) + gammaP_N(y(2)))*y(3); %stressed population ode
+dydt(4)=((r_2*(K-y(1))/K) + psiP_R(y(4)))*y(3) + deltaP_S(y(3))*y(2);  %recovered population ode
+
+end
